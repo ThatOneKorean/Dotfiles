a2806f9de26a0578e526c65854e525b3d62b32bc
thatonekorean <thatonekorean21@gmail.com>
2025-10-16 16:00:25 -0600

    added wlogout files

--- laptops/deli/scripts/update_repo.sh
+++ laptops/deli/scripts/update_repo.sh
@@ -6,3 +6,4 @@ rm -r ~/Dotfiles/laptops/deli/.config/*
 cp -r ~/.config/waybar ~/Dotfiles/laptops/deli/.config/
 cp -r ~/.config/hypr ~/Dotfiles/laptops/deli/.config/
 cp -r ~/.config/kitty ~/Dotfiles/laptops/deli/.config/
+cp -r ~/.config/wlogout ~/Dotfiles/laptops/deli/.config/
